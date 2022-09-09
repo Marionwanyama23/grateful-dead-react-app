9e22fa166ec5e2f7dc0c3b01942b16a7b2c546bf
+ <+>
2022-09-09 13:00:45 +0300

    commit

--- src/components/FormSongs.js
+++ src/components/FormSongs.js
@@ -4,7 +4,6 @@ import React, {useState} from 'react'
 function Form({addNewSong}){
     const [name, setName] = useState("")
 
-    
     function handleSubmit(event){
         event.preventDefault()
         fetch('http://localhost:9393/songs/', {
